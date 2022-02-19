c6717d8c9ae0b5a8ef8baec8a791aa5ade53865c
AnSeonJeong <asj081@naver.com>
2022-02-20 04:44:51 +0900

    greeting

---
+++ js/greeting.js
@@ -0,0 +1,37 @@
+const loginForm = document.querySelector("#login_form");
+const loginInput = document.querySelector("#login_form input");
+const logoutBtn = document.querySelector(".logout");
+const greeting = document.querySelector(".greeting");
+
+const USERNAME = "username"
+const HIDDEN_CLASSNAME = "hidden"
+
+function LoginSubmit(e) {
+    e.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME, username);
+    showGreeting(username);
+}
+
+const savedUsername = localStorage.getItem(USERNAME);
+
+function showGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function handleLogout() {
+    localStorage.removeItem(USERNAME);
+    window.location.reload();
+}
+
+
+if(savedUsername===null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", LoginSubmit);
+} else {
+    showGreeting(savedUsername);
+    logoutBtn.addEventListener("click", handleLogout);
+}
+
